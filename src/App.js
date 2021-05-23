8e56915e30294fe402a48e6331820b2fa78e8933
Mayrbek <mayrbek.jamaldinov@gmail.com>
2021-05-23 13:28:49 +0400

    git@github.com:harlok-m-code/Repos.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import axios from 'axios';
+import React from 'react';
+import Data from './components/Data';
+import Table from './components/Table';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const [data,setData] = React.useState([]);
+  const [check,setChek] = React.useState(true);
+  const [rowItem,setRowItem] = React.useState('');
+
+  const sortData = (items) => {
+
+
+
+    if(check){
+      const sorte = data.sort((a,b)=> {
+        return a[items] > b[items] ? 1 : -1
+      });
+      setData(sorte);
+      setChek(!check);
+    }else{
+      const sorte = data.reverse((a,b)=> {
+        return a[items] > b[items] ? 1 : -1
+      });
+      setData(sorte);
+      setChek(!check);
+    }
+  }
+
+  React.useEffect(()=>{
+    axios.get('http://localhost:3001/db.json').then(
+      ({data})=> {
+        setData(data);
+      }
+    )
+  },[])
+
+
+  const detRow = (row) => {
+    setRowItem(row);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Table data={data} sortData={sortData} detRow={detRow}></Table>
+      <Data rowItem={rowItem}></Data>
     </div>
   );
 }
 
 export default App;
+
+axios.get().then(({data})=>{
+  
+})
